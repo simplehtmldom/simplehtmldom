75a8171f7f18307897120bb6a17b7e8cb6f9f528
John Schlick <john_schlick@users.noreply.sourceforge.net>
2011-07-26 20:11:35 +0000

    Change the version number to 1.5 just for clarity.

--- simple_html_dom.php
+++ simple_html_dom.php
@@ -30,7 +30,7 @@
  * @author S.C. Chen <me578022@gmail.com>
  * @author John Schlick
  * @author Rus Carroll
- * @version 1.11 ($Rev$)
+ * @version 1.5 ($Rev$)
  * @package PlaceLocalInclude
  * @subpackage simple_html_dom
  */
