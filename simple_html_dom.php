5d92caa172de295e9068c451a54f9abd73074fe5
John Schlick <john_schlick@users.noreply.sourceforge.net>
2013-05-14 14:03:03 +0000

    swap noice stripping things for better functionality.

--- simple_html_dom.php
+++ simple_html_dom.php
@@ -1060,10 +1060,10 @@ class simple_html_dom
 
 		// prepare
 		$this->prepare($str, $lowercase, $stripRN, $defaultBRText, $defaultSpanText);
-		// strip out comments
-		$this->remove_noise("'<!--(.*?)-->'is");
 		// strip out cdata
 		$this->remove_noise("'<!\[CDATA\[(.*?)\]\]>'is", true);
+		// strip out comments
+		$this->remove_noise("'<!--(.*?)-->'is");
 		// Per sourceforge http://sourceforge.net/tracker/?func=detail&aid=2949097&group_id=218559&atid=1044037
 		// Script tags removal now preceeds style tag removal.
 		// strip out <script> tags
