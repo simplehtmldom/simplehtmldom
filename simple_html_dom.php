0d9e1001afdfb5ea0058d168d64e8421aa19d131
logmanoriginal <logmanoriginal@users.noreply.sourceforge.net>
2018-11-29 18:39:41 +0100

    Improve documentation and readability for $self_closing_tags
    - Add documentation
    - Sort elements by name
    - Split list to one element per line

--- simple_html_dom.php
+++ simple_html_dom.php
@@ -1009,8 +1009,30 @@ class simple_html_dom
 	protected $default_br_text = "";
 	public $default_span_text = "";
 
-	// use isset instead of in_array, performance boost about 30%...
-	protected $self_closing_tags = array('img'=>1, 'br'=>1, 'input'=>1, 'meta'=>1, 'link'=>1, 'hr'=>1, 'base'=>1, 'embed'=>1, 'spacer'=>1);
+	/**
+	 * Defines a list of self-closing tags (Void elements) according to the HTML
+	 * Specification
+	 *
+	 * _Remarks_:
+	 * - Use `isset()` instead of `in_array()` on array elements to boost
+	 * performance about 30%
+	 * - Sort elements by name for better readability!
+	 *
+	 * @link https://www.w3.org/TR/html HTML Specification
+	 * @link https://www.w3.org/TR/html/syntax.html#void-elements Void elements
+	 */
+	protected $self_closing_tags = array(
+		'base'=>1,
+		'br'=>1,
+		'embed'=>1,
+		'hr'=>1,
+		'img'=>1,
+		'input'=>1,
+		'link'=>1,
+		'meta'=>1,
+		'spacer'=>1
+	);
+
 	protected $block_tags = array('root'=>1, 'body'=>1, 'form'=>1, 'div'=>1, 'span'=>1, 'table'=>1);
 	// Known sourceforge issue #2977341
 	// B tags that are not closed cause us to return everything to the end of the document.
