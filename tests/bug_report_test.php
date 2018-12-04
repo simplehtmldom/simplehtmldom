43f247e2bba80a459346beb68d192da35f8f3aee
logmanoriginal <logmanoriginal@users.noreply.sourceforge.net>
2018-12-04 19:32:23 +0100

    Add test for bug #154
    The parser incorrectly removes everything between "{" and "}" in an
    attempt to remove "Smarty Scripts" from the DOM. This causes regular
    text to be removed if it contains curly braces.

--- tests/bug_report_test.php
+++ tests/bug_report_test.php
@@ -16,6 +16,46 @@ class bug_report_test extends TestCase {
 	}
 
 	/**
+	 * Bug #154 (Fatal error: Call to a member function find() on null)
+	 *
+	 * The parser incorrectly removes everything between `{` and `}` attempting
+	 * to remove "Smarty Scripts" from the DOM. This causes regular text to be
+	 * removed as well, if it contains curly braces.
+	 *
+	 * **Example Code**:
+	 *
+	 * ```HTML
+	 * <div class="before"></div>
+	 *   <p>{PHP Simple HTML DOM Parser</p>
+	 *   <p>{A PHP based DOM parser}</p>
+	 * <div id="after"></div>
+	 * ```
+	 *
+	 * @link https://sourceforge.net/p/simplehtmldom/bugs/154/ Bug #154
+	 * @link https://www.smarty.net Smarty
+	 */
+	public function test_bug_154() {
+		$doc = <<<HTML
+<p>{PHP Simple HTML DOM Parser</p>
+<p>{A PHP based DOM parser}</p>
+HTML;
+
+		$this->html->load($doc);
+
+		$this->assertCount(2, $this->html->find('p'));
+
+		$this->assertEquals(
+			'{PHP Simple HTML DOM Parser',
+			$this->html->find('p', 0)->innertext
+		);
+
+		$this->assertEquals(
+			'{A PHP based DOM parser}',
+			$this->html->find('p', 1)->innertext
+		);
+	}
+
+	/**
 	 * Bug #160 (Parsing fails with '<-' + '/' symbols combination in string)
 	 *
 	 * **Example Code**:
