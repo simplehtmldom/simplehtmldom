9df7f46ea565524c2539884252283531d575671a
logmanoriginal <logmanoriginal@users.noreply.sourceforge.net>
2018-12-06 15:31:08 +0100

    Remove script tags before replacing newline characters
    Script tags containing comments break when removing newline characters.
    
    This commit changes the order of operation to remove script tags before
    replacing newline characters.
    
    Note:
    
    Whitespace at start and end of the document string is removed before
    any other operation. This impacts the total document size of
    simple_html_dom::original_size.
    
    References #121

--- simple_html_dom.php
+++ simple_html_dom.php
@@ -1392,16 +1392,27 @@ class simple_html_dom
 		global $debug_object;
 
 		// prepare
-		$this->prepare($str, $lowercase, $stripRN, $defaultBRText, $defaultSpanText);
-		// strip out cdata
-		$this->remove_noise("'<!\[CDATA\[(.*?)\]\]>'is", true);
-		// strip out comments
-		$this->remove_noise("'<!--(.*?)-->'is");
+		$this->prepare($str, $lowercase, $defaultBRText, $defaultSpanText);
+
 		// Per sourceforge http://sourceforge.net/tracker/?func=detail&aid=2949097&group_id=218559&atid=1044037
 		// Script tags removal now preceeds style tag removal.
 		// strip out <script> tags
 		$this->remove_noise("'<\s*script[^>]*[^/]>(.*?)<\s*/\s*script\s*>'is");
 		$this->remove_noise("'<\s*script\s*>(.*?)<\s*/\s*script\s*>'is");
+
+		// strip out the \r \n's if we are told to.
+		if ($stripRN) {
+			$this->doc = str_replace("\r", " ", $this->doc);
+			$this->doc = str_replace("\n", " ", $this->doc);
+
+			// set the length of content since we have changed it.
+			$this->size = strlen($this->doc);
+		}
+
+		// strip out cdata
+		$this->remove_noise("'<!\[CDATA\[(.*?)\]\]>'is", true);
+		// strip out comments
+		$this->remove_noise("'<!--(.*?)-->'is");
 		// strip out <style> tags
 		$this->remove_noise("'<\s*style[^>]*[^/]>(.*?)<\s*/\s*style\s*>'is");
 		$this->remove_noise("'<\s*style\s*>(.*?)<\s*/\s*style\s*>'is");
@@ -1492,25 +1503,13 @@ class simple_html_dom
 	}
 
 	// prepare HTML data and init everything
-	protected function prepare($str, $lowercase=true, $stripRN=true, $defaultBRText=DEFAULT_BR_TEXT, $defaultSpanText=DEFAULT_SPAN_TEXT)
+	protected function prepare($str, $lowercase=true, $defaultBRText=DEFAULT_BR_TEXT, $defaultSpanText=DEFAULT_SPAN_TEXT)
 	{
 		$this->clear();
 
-		// set the length of content before we do anything to it.
-		$this->size = strlen($str);
-		// Save the original size of the html that we got in.  It might be useful to someone.
-		$this->original_size = $this->size;
-
-		//before we save the string as the doc...  strip out the \r \n's if we are told to.
-		if ($stripRN) {
-			$str = str_replace("\r", " ", $str);
-			$str = str_replace("\n", " ", $str);
-
-			// set the length of content since we have changed it.
-			$this->size = strlen($str);
-		}
-
-		$this->doc = $str;
+		$this->doc = trim($str);
+		$this->size = strlen($this->doc);
+		$this->original_size = $this->size; // Save the original size of the html that we got in.  It might be useful to someone.
 		$this->pos = 0;
 		$this->cursor = 1;
 		$this->noise = array();
