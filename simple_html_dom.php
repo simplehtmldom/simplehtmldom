simplehtmldom/simplehtmldom

commit fee87f0dfe12392598ced20a0f07ceb6449f1031

oops.  Forgot to unmodify the Paperg part of the project.

simple_html_dom.php
@@ -67,9 +67,9 @@ function file_get_html($url, $use_include_path = false, $context=null, $offset =
     // We DO force the tags to be terminated.
     $dom = new simple_html_dom(null, $lowercase, $forceTagsClosed, $target_charset, $defaultBRText);
     // For sourceforge users: uncomment the next line and comment the retreive_url_contents line 2 lines down if it is not already done.
-    //$contents = file_get_contents($url, $use_include_path, $context, $offset, $maxLen);
+    $contents = file_get_contents($url, $use_include_path, $context, $offset);
     // Paperg - use our own mechanism for getting the contents as we want to control the timeout.
-    $contents = retrieve_url_contents($url);
+//    $contents = retrieve_url_contents($url);
     if (empty($contents))
     {
         return false;

commit f5aa52b7641c9a0aaee0d885984ac737afc9ab57

Restore functionality of $maxLen in file_get_html
This parameter was broken since fee87f0.

$maxLen specifies the maximum number of characters loaded from $url.
The default value is MAX_FILE_SIZE if $maxLen = -1 (actually if it is
smaller than or equal to zero).

MAX_FILE_SIZE also no longer prevents loading larger documents if
$maxLen specifies a bigger value.

simple_html_dom.php
@@ -74,13 +74,16 @@ define('HDOM_SMARTY_AS_TEXT', 1);
 // $maxlen is defined in the code as PHP_STREAM_COPY_ALL which is defined as -1.
 function file_get_html($url, $use_include_path = false, $context=null, $offset = 0, $maxLen=-1, $lowercase = true, $forceTagsClosed=true, $target_charset = DEFAULT_TARGET_CHARSET, $stripRN=true, $defaultBRText=DEFAULT_BR_TEXT, $defaultSpanText=DEFAULT_SPAN_TEXT)
 {
+	// Ensure maximum length is greater than zero
+	if($maxLen <= 0) { $maxLen = MAX_FILE_SIZE; }
+
 	// We DO force the tags to be terminated.
 	$dom = new simple_html_dom(null, $lowercase, $forceTagsClosed, $target_charset, $stripRN, $defaultBRText, $defaultSpanText);
 	// For sourceforge users: uncomment the next line and comment the retrieve_url_contents line 2 lines down if it is not already done.
-	$contents = file_get_contents($url, $use_include_path, $context, $offset);
+	$contents = file_get_contents($url, $use_include_path, $context, $offset, $maxLen);
 	// Paperg - use our own mechanism for getting the contents as we want to control the timeout.
 	//$contents = retrieve_url_contents($url);
-	if (empty($contents) || strlen($contents) > MAX_FILE_SIZE)
+	if (empty($contents) || strlen($contents) > $maxLen)
 	{
 		return false;
 	}