427b882383854524ae51871f3ff791279a9c8b6f
logmanoriginal <logmanoriginal@users.noreply.sourceforge.net>
2018-12-08 21:45:31 +0100

    Add build script to automate releases
    release.sh specifies a build script to automatically generate release
    files based on the current commit tag. This is a two-stage process:
    
    1) Tag current version: "git tag x.y", where x is the major and y the
         minor version number. For example: "git tag 1.6"
    
    2) Build release file: "sh release.sh". For the example above, this
         will build "simplehtmldom_1_6.zip"
    
    Note:
    
    All ocurrences of "$Rev$" are replaced by "Rev. x.y (n)", where "x" is
    the major and "y" the minor version number and "n" is the total number
    of commits in the current branch.

---
+++ release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This file automates the release process based on the tag of the current commit
+#
+# 1) Tag current version: "git tag x.y", where x is the major and y the minor
+#   version number. For example: "git tag 1.6"
+#
+# 2) Build release file: "sh release.sh". For the example above, this will build
+#   "simplehtmldom_1_6.zip"
+
+tag=$(git tag -l --points-at HEAD)
+
+# Archive file
+prefix="simplehtmldom_"
+version=$(echo "$tag" | tr . _)
+
+# Keyword substitution in files
+marker="\\\$Rev\\\$"
+replacement="Rev. $tag ($(git rev-list --count --all))"
+
+# Build archive
+if [ "$version" ]; then
+  # Inject version information to all files (limit to file type!)
+  find . -name '*.php' -exec sed -i -e "s/$marker/$replacement/g" {} \;;
+  find . -name '*.htm' -exec sed -i -e "s/$marker/$replacement/g" {} \;;
+  # Create stash commit (otherwise git archive won't work)
+  stash=$(git stash create);
+  git archive --format=zip --output="$prefix$version".zip --worktree-attributes "$stash";
+  # Clenup
+  git checkout .;
+  git gc --prune;
+else
+  echo "Your commit is not tagged!";
+fi;
