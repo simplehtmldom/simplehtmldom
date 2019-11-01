simplehtmldom/simplehtmldom

commit b50d97b44b4968d66afeed39bff02ee04f7d09e4

Add full support for "text", "comment" and "cdata" selectors
* Do not strip comments before parsing the document

  Comments are currently being removed from the input document before
  parsing the DOM. This makes it impossible to handle comments like
  nodes.

  With this commit, comments are no longer removed.

* Add node type 'HDOM_TYPE_CDATA' for CDATA elements

  CDATA sections were removed from the input document before parsing
  the DOM. This made it impossible to handle CDATA like nodes. It is
  also not correct to ignore these elements when generating plaintext
  as the content of CDATA is supposed to be handled literally.

* Return innertext and outertext for text, comment and cdata

  text:

    Text elements may appear anywhere in the document. Any element
    that contains text is automatically returned as text node. This
    allows users to address any text in the document by index.

    Text is always included in plaintext output.

    For example:

      HTML:     '<p>Hello, World!</p>'

      Selector: '$html->find('text', 0)->innertext;'
      Output:   'Hello, World!'

      Selector: '$html->find('text', 0)->outertext;'
      Output:   '<p>Hello, World!</p>'

      Selector: '$html->find('text', 0)->plaintext;'
      Output:   'Hello, World!'

    2nd example:

      HTML:     'Hello, World!'

      Selector: *any of the above*
      Output:   'Hello, World!'

  cdata:

    CDATA elements may appear anywhere in the document. CDATA elements
    are detected by the parser and handled specifically. This allows
    users to address CDATA sections by index and decide whether to get
    the innertext or outertext representation.

    Note that CDATA sections are included in plaintext output.

    For example:

      HTML:     '<![CDATA[<p>Hello, World!</p>]]>'

      Selector: '$html->find('cdata', 0)->innertext;'
      Output:   '<p>Hello, World!</p>'

      Selector: '$html->find('cdata', 0)->outertext;'

HtmlDocument.php
@@ -164,8 +164,6 @@ class HtmlDocument
 			$this->size = strlen($this->doc);
 		}
 
-		// strip out comments
-		$this->remove_noise("'<!--(.*?)-->'is");
 		// strip out <style> tags
 		$this->remove_noise("'<\s*style[^>]*[^/]>(.*?)<\s*/\s*style\s*>'is");
 		$this->remove_noise("'<\s*style\s*>(.*?)<\s*/\s*style\s*>'is");
@@ -589,30 +587,78 @@ class HtmlDocument
 
 		if (isset($tag[0]) && $tag[0] === '!') { // Doctype, CData, Comment
 			if (isset($tag[2]) && $tag[1] === '-' && $tag[2] === '-') { // Comment ("<!--")
+
+				// Go back until $tag only contains start of comment "!--".
+				while (strlen($tag) > 3) {
+					$this->char = $this->doc[--$this->pos]; // previous
+					$tag = substr($tag, 0, strlen($tag) - 1);
+				}
+
 				$node->nodetype = HtmlNode::HDOM_TYPE_COMMENT;
 				$node->tag = 'comment';
+
+				$data = '';
+
+				// There is a rare chance of empty comment: "<!---->"
+				// In which case the current char is the first "-" of the end tag
+				// But the comment could also just be a dash: "<!----->"
+				while(true) {
+					// Copy until first char of end tag
+					$data .= $this->copy_until_char('-');
+
+					// Look ahead in the document, maybe we are at the end
+					if (($this->pos + 3) > $this->size) { // End of document
+						break;
+					} elseif (substr($this->doc, $this->pos, 3) === '-->') { // end
+						$data .= $this->copy_until_char('>');
+						break;
+					}
+
+					$data .= $this->char;
+					$this->char = (++$this->pos < $this->size) ? $this->doc[$this->pos] : null; // next
+				}
+
+				$tag .= $data;
+
+				// Comment starts after "!--" and ends before "--" (5 chars total)
+				$node->_[HtmlNode::HDOM_INFO_INNER] = substr($tag, 3, strlen($tag) - 5);
 			} elseif (substr($tag, 1, 7) === '[CDATA[') {
+
+				// Go back until $tag only contains start of cdata "![CDATA[".
+				while (strlen($tag) > 8) {
+					$this->char = $this->doc[--$this->pos]; // previous
+					$tag = substr($tag, 0, strlen($tag) - 1);
+				}
+
 				// CDATA can contain HTML stuff, need to find closing tags first
-				$node->nodetype = HtmlNode::HDOM_TYPE_UNKNOWN; // fixme
+				$node->nodetype = HtmlNode::HDOM_TYPE_CDATA;
 				$node->tag = 'cdata';
 
 				$data = '';
 
-				while (($part = $this->copy_until_char(']')) !== '') {
-					$data .= $part . ']';
+				// There is a rare chance of empty CDATA: "<[CDATA[]]>"
+				// In which case the current char is the first "[" of the end tag
+				// But the CDATA could also just be a bracket: "<[CDATA[]]]>"
+				while(true) {
+					// Copy until first char of end tag
+					$data .= $this->copy_until_char(']');
 
-					$this->char = (++$this->pos < $this->size) ? $this->doc[$this->pos] : null; // next
-
-					if($this->char === ']') { // potential end of section
-						$part = $this->copy_until_char('>');
-						$data .= $part;
-						if ($part === ']') { // end of section ("]]>")
-							break;
-						}
+					// Look ahead in the document, maybe we are at the end
+					if (($this->pos + 3) > $this->size) { // End of document
+						break;
+					} elseif (substr($this->doc, $this->pos, 3) === ']]>') { // end
+						$data .= $this->copy_until_char('>');
+						break;
 					}
+
+					$data .= $this->char;
+					$this->char = (++$this->pos < $this->size) ? $this->doc[$this->pos] : null; // next
 				}
 
 				$tag .= $data;
+
+				// CDATA starts after "![CDATA[" and ends before "]]" (10 chars total)
+				$node->_[HtmlNode::HDOM_INFO_INNER] = substr($tag, 8, strlen($tag) - 10);
 			} else { // Unknown
 				$node->nodetype = HtmlNode::HDOM_TYPE_UNKNOWN;
 				$node->tag = 'unknown';

HtmlNode.php
@@ -32,6 +32,7 @@ class HtmlNode
 	const HDOM_TYPE_ENDTAG = 4;
 	const HDOM_TYPE_ROOT = 5;
 	const HDOM_TYPE_UNKNOWN = 6;
+	const HDOM_TYPE_CDATA = 7;
 
 	const HDOM_QUOTE_DOUBLE = 0;
 	const HDOM_QUOTE_SINGLE = 1;
@@ -377,14 +378,16 @@ class HtmlNode
 			$ret = '';
 		} elseif (strtolower($this->tag) === 'style') {
 			$ret = '';
+		} elseif ($this->nodetype === self::HDOM_TYPE_COMMENT) {
+			$ret = '';
+		} elseif ($this->nodetype === self::HDOM_TYPE_CDATA) {
+			$ret = $this->_[self::HDOM_INFO_INNER];
+		} elseif ($this->nodetype === self::HDOM_TYPE_UNKNOWN) {
+			$ret = '';
 		} elseif (isset($this->_[self::HDOM_INFO_INNER])) {
 			$ret = $this->_[self::HDOM_INFO_INNER];
 		} elseif ($this->nodetype === self::HDOM_TYPE_TEXT) {
 			$ret = $this->_[self::HDOM_INFO_TEXT];
-		} elseif ($this->nodetype === self::HDOM_TYPE_COMMENT) {
-			$ret = '';
-		} elseif ($this->nodetype === self::HDOM_TYPE_UNKNOWN) {
-			$ret = '';
 		}
 
 		if (is_null($this->nodes)) {
@@ -597,6 +600,41 @@ class HtmlNode
 
 			// Skip root nodes
 			if(!$node->parent) {
+				unset($node);
+				continue;
+			}
+
+			// Handle 'text' selector
+			if($pass && $tag === 'text') {
+
+				if($node->tag === 'text') {
+					$ret[array_search($node, $this->dom->nodes, true)] = 1;
+				}
+
+				if(isset($node->_[self::HDOM_INFO_INNER])) {
+					$ret[$node->_[self::HDOM_INFO_BEGIN]] = 1;
+				}
+
+				unset($node);
+				continue;
+
+			}
+
+			// Handle 'cdata' selector
+			if($pass && $tag === 'cdata') {
+
+				if($node->tag === 'cdata') {
+					$ret[$node->_[self::HDOM_INFO_BEGIN]] = 1;
+				}
+
+				unset($node);
+				continue;
+
+			}
+
+			// Handle 'comment'
+			if($pass && $tag === 'comment' && $node->tag === 'comment') {
+				$ret[$node->_[self::HDOM_INFO_BEGIN]] = 1;
 				unset($node);
 				continue;
 			}
@@ -1002,7 +1040,7 @@ class HtmlNode
 			case 'outertext': return $this->_[self::HDOM_INFO_OUTER] = $value;
 			case 'innertext':
 				if (isset($this->_[self::HDOM_INFO_TEXT])) {
-					return $this->_[self::HDOM_INFO_TEXT] = $value;
+					$this->_[self::HDOM_INFO_TEXT] = '';
 				}
 				return $this->_[self::HDOM_INFO_INNER] = $value;
 		}

tests/cdata_test.php
@@ -1,0 +1,52 @@
+<?php
+require_once __DIR__ . '/../simple_html_dom.php';
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Checks if the parser properly handles CDATA sections
+ */
+class cdata_test extends TestCase {
+
+	private $html;
+
+	protected function setUp()
+	{
+		$this->html = new simple_html_dom;
+	}
+
+	/**
+	 * @dataProvider dataProvider_for_cdata_should_parse
+	 */
+	public function test_cdata_should_parse($expected, $doc)
+	{
+		$this->html->load($doc);
+		$this->assertEquals($expected, $this->html->find('cdata', 0)->innertext);
+		$this->assertEquals($doc, $this->html->save());
+	}
+
+	public function dataProvider_for_cdata_should_parse()
+	{
+		return array(
+			'empty' => array(
+				'',
+				'<![CDATA[]]>',
+			),
+			'whitespace' => array(
+				' ',
+				'<![CDATA[ ]]>',
+			),
+			'brackets' => array(
+				']][[',
+				'<![CDATA[]][[]]>',
+			),
+			'html' => array(
+				'<p>Hello, World!</p>',
+				'<![CDATA[<p>Hello, World!</p>]]>',
+			),
+			'comment' => array(
+				'<!-- Hello, World! -->',
+				'<![CDATA[<!-- Hello, World! -->]]>'
+			),
+		);
+	}
+}

tests/comment_test.php
@@ -1,0 +1,52 @@
+<?php
+require_once __DIR__ . '/../simple_html_dom.php';
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Checks if the parser properly handles comments
+ */
+class comment_test extends TestCase {
+
+	private $html;
+
+	protected function setUp()
+	{
+		$this->html = new simple_html_dom;
+	}
+
+	/**
+	 * @dataProvider dataProvider_for_comment_should_parse
+	 */
+	public function test_comment_should_parse($expected, $doc)
+	{
+		$this->html->load($doc);
+		$this->assertEquals($expected, $this->html->find('comment', 0)->innertext);
+		$this->assertEquals($doc, $this->html->save());
+	}
+
+	public function dataProvider_for_comment_should_parse()
+	{
+		return array(
+			'empty' => array(
+				'',
+				'<!---->',
+			),
+			'whitespace' => array(
+				' ',
+				'<!-- -->',
+			),
+			'brackets' => array(
+				']][[',
+				'<!--]][[-->',
+			),
+			'html' => array(
+				'<p>Hello, World!</p>',
+				'<!--<p>Hello, World!</p>-->',
+			),
+			'cdata' => array(
+				'<![CDATA[Hello, World!]]>',
+				'<!--<![CDATA[Hello, World!]]>-->'
+			),
+		);
+	}
+}

tests/css_selector_test.php
@@ -544,4 +544,97 @@ HTML;
 	}
 
 	#endregion Pseudo Classes
+
+	/**
+	 * "comment", "cdata" and "text" selectors are specific to this parser. They
+	 * allow users to directly address these nodes and extract useful information.
+	 *
+	 * @dataProvider dataProvider_for_find_should_work_with_special_selector
+	 */
+	public function test_find_should_work_with_special_selector($selector, $expected, $doc, $message)
+	{
+		$this->html->load($doc);
+		$this->assertEquals($expected, $this->html->find($selector, 0)->innertext, $message);
+		$this->assertEquals($doc, $this->html->save());
+	}
+
+	public function dataProvider_for_find_should_work_with_special_selector()
+	{
+		$data = array(
+			'text without elements' => array(
+				'text',
+				'Hello, World!',
+				'Hello, World!',
+				'find should return text without elements'
+			),
+			'text outside html' => array(
+				'text',
+				'Hello, World!',
+				'Hello, World!<html></html>',
+				'find should return text outside html'
+			),
+			'text inside element' => array(
+				'text',
+				'Hello, World!',
+				'<html>Hello, World!</html>',
+				'find should return text inside element'
+			),
+			'text between elements' => array(
+				'text',
+				'Hello, World!',
+				'<html><head></head>Hello, World!<body></body></html>',
+				'find should return text between elements'
+			),
+			'cdata without elements' => array(
+				'cdata',
+				'Hello, World!',
+				'<![CDATA[Hello, World!]]>',
+				'find should return cdata elements'
+			),
+			'cdata outside html' => array(
+				'cdata',
+				'Hello, World!',
+				'<![CDATA[Hello, World!]]><html></html>',
+				'find should return cdata elements'
+			),
+			'cdata inside element' => array(
+				'cdata',
+				'Hello, World!',
+				'<html><![CDATA[Hello, World!]]></html>',
+				'find should return cdata elements'
+			),
+			'cdata between elements' => array(
+				'cdata',
+				'Hello, World!',
+				'<html><head></head><![CDATA[Hello, World!]]><body></body></html>',
+				'find should return cdata elements'
+			),
+			'comment without elements' => array(
+				'comment',
+				'Hello, World!',
+				'<!--Hello, World!-->',
+				'find should return comments'
+			),
+			'comment outside html' => array(
+				'comment',
+				'Hello, World!',
+				'<!--Hello, World!--><html></html>',
+				'find should return comments'
+			),
+			'comment inside element' => array(
+				'comment',
+				'Hello, World!',
+				'<html><!--Hello, World!--></html>',
+				'find should return comments'
+			),
+			'comment between elements' => array(
+				'comment',
+				'Hello, World!',
+				'<html><head></head><!--Hello, World!--><body></body></html>',
+				'find should return comments'
+			)
+		);
+
+		return $data;
+	}
 }

tests/simple_html_dom_node_test.php
@@ -138,7 +138,7 @@ EOD;
 
 	public function test_innertext_should_return_comment()
 	{
-		$expected = '<!--For your information-->';
+		$expected = 'For your information';
 		$doc = '<html><!--For your information--></html>';
 
 		$this->html->load($doc);
@@ -327,10 +327,10 @@ EOD;
 		$this->assertEquals($expected, $this->html->root->text());
 	}
 
-	public function test_text_should_not_include_cdata()
+	public function test_text_should_include_cdata_content()
 	{
-		$expected = 'PHP Simple HTML DOM Parser';
-		$doc = '<![CDATA[<html></html>]]><h1>PHP Simple HTML DOM Parser</h1>';
+		$expected = '<?php Simple HTML DOM Parser';
+		$doc = '<h1><![CDATA[<?php]]> Simple HTML DOM Parser</h1>';
 
 		$this->html->load($doc);
 

commit a05bea47db888c4696bc80bc737181550f0340f0

Fix duplicate noise restoration halts the script
This issue was introduced by b50d97b for which CDATA and Comments
store the same information twice. However, when restore_noise() is
used on either one of the elements, the other cannot be restored
because restore_noise() immediately unsets the restored element.

This document failed with the previous commit:

  <!-- <style></style> -->

HtmlDocument.php
@@ -597,6 +597,7 @@ class HtmlDocument
 				}
 
 				$tag .= $data;
+				$tag = $this->restore_noise($tag);
 
 				// Comment starts after "!--" and ends before "--" (5 chars total)
 				$node->_[HtmlNode::HDOM_INFO_INNER] = substr($tag, 3, strlen($tag) - 5);
@@ -635,6 +636,7 @@ class HtmlDocument
 				}
 
 				$tag .= $data;
+				$tag = $this->restore_noise($tag);
 
 				// CDATA starts after "![CDATA[" and ends before "]]" (10 chars total)
 				$node->_[HtmlNode::HDOM_INFO_INNER] = substr($tag, 8, strlen($tag) - 10);